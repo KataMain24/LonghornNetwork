5b2b6e64f2003288985ac904a5cb7bdca0568650
Kody Keo <kody23232@gmail.com>
2025-11-17 21:41:00 -0600

    StudentGraph class created

---
+++ src/StudentGraph.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+/**
+ * A graph data structure representing connections between students based on
+ * their connection strength. Each edge in the graph is weighted and reciprocal.
+ */
+public class StudentGraph {
+
+}
